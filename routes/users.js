6f3eace26b655bbaeed79f76a6e2fd5426626789
Tuhin Poddar <tuhinpoddar114@gmail.com>
2023-12-18 11:02:33 +0530

    mongodb setup

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,19 @@
 var express = require("express");
 var router = express.Router();
+const mongoose = require("mongoose");
+const plm = require("passport-local-mongoose");
 
-/* GET users listing. */
-router.get("/", function (req, res, next) {
-  res.send("respond with a resource");
+mongoose.connect("mongodb://localhost:27017/Instagram");
+
+const userSchema = mongoose.Schema({
+  username: String,
+  name: String,
+  email: String,
+  password: String,
+  profileImage: String,
+  posts: [{ type: mongoose.Schema.Types.ObjectId, ref: "post" }],
 });
 
-module.exports = router;
+userSchema.plugin(plm);
+
+module.exports = mongoose.model("user", userSchema);
